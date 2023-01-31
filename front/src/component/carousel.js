28b125fcf6ca7e443488ae5b6e74ed4dc3b513be
dallas <dallas.wty@gmail.com>
2023-01-31 19:01:25 +0900

    carousel click and move

--- front/src/component/carousel.js
+++ front/src/component/carousel.js
@@ -1,3 +1,4 @@
+import Slider from 'react-touch-drag-slider';
 import style from '../css/carousel.module.css';
 import img1 from '../img/1.jpg';
 import img2 from '../img/2.jpg';
@@ -5,14 +6,32 @@ import img3 from '../img/3.jpg';
 
 function Carousel(){
     const window = {width:"640px", height:"360px", overflow:'hidden'}
-
+    const images = [img1, img2, img3];
     return (
+        // <div style={window}>
+        //     <div style={{display: "flex"}}>
+        //         <img className={style.img} alt="thumbnail_1" src={img1}></img>
+        //         <img className={style.img} alt="thumbnail_2" src={img2}></img>
+        //         <img className={style.img} alt="thumbnail_3" src={img3}></img>
+        //     </div>
+        // </div>
         <div style={window}>
-            <div style={{display: "flex"}}>
-                <img className={style.img} alt="thumbnail_1" src={img1}></img>
-                <img className={style.img} alt="thumbnail_2" src={img2}></img>
-                <img className={style.img} alt="thumbnail_3" src={img3}></img>
-            </div>
+            <Slider
+            onSlideComplete={(i) => {
+                console.log('finished dragging, current slide is', i)
+            }}
+            onSlideStart={(i) => {
+                console.log('started dragging on slide', i)
+            }}
+            activeIndex={0}
+            threshHold={100}
+            transition={0.5}
+            scaleOnDrag={true}
+            >
+            {images.map((image, idx) => (
+                <img src={image} key={idx} alt={idx} className={style.img} />
+            ))}
+            </Slider>
         </div>
     )
 }
