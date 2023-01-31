87c77852005e50eb220d7d5af847e3192f6dfc90
dallas <dallas.wty@gmail.com>
2023-01-31 18:42:27 +0900

    carousel setting

--- front/src/component/carousel.js
+++ front/src/component/carousel.js
@@ -1,10 +1,19 @@
+import style from '../css/carousel.module.css';
+import img1 from '../img/1.jpg';
+import img2 from '../img/2.jpg';
+import img3 from '../img/3.jpg';
+
 function Carousel(){
-    let size = {width:"640px", height:"360px"};
+    const window = {width:"640px", height:"360px", overflow:'hidden'}
 
     return (
-        <div style={size}>
-            <h2>Carousel</h2>
-        </div>>
+        <div style={window}>
+            <div style={{display: "flex"}}>
+                <img className={style.img} alt="thumbnail_1" src={img1}></img>
+                <img className={style.img} alt="thumbnail_2" src={img2}></img>
+                <img className={style.img} alt="thumbnail_3" src={img3}></img>
+            </div>
+        </div>
     )
 }
 
