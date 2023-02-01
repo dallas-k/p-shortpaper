a3b0fe4c9d093fa389e675a2338be4ccfb27be93
dallas <dallas.wty@gmail.com>
2023-02-01 13:21:01 +0900

    footer

--- front/src/component/footer.js
+++ front/src/component/footer.js
@@ -1,11 +1,18 @@
+import './css/footer.css';
+
 function Footer() {
     return (
-        <div className='footer'>
-            <h2>짧은대본</h2>
-            <p>본 사이트는 짧은대본의 팬페이지로서, 영리적 수익을 추구하지 않습니다</p>
-            <p>Copyright(C) 2023 Youtube Channel [Short_Paper] All Rights Reserved</p>
-            <p>Made by: Dallas | dallas.wty@gmail.com</p>
-        </div>
+        <footer>
+            <div className='footer'>
+                <h2>짧은대본</h2>
+                <div className='footer-desc'>
+                    <p>본 사이트는 짧은대본의 팬페이지로서, 영리적 수익을 추구하지 않습니다</p>
+                    <p>Copyright(C) 2023 Youtube Channel [Short_Paper] All Rights Reserved</p>
+                    <p>Made by: <span>Dallas</span> | dallas.wty@gmail.com</p>
+                </div>
+            </div>
+        </footer>
+        
     )
 }
 
