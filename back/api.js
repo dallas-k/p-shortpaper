3d0222dd9a127bfb1fe238806daec0f9f6221d7c
dallas <dallas.wty@gmail.com>
2023-02-01 18:15:41 +0900

    actor api

--- back/api.js
+++ back/api.js
@@ -1,14 +1,36 @@
+const fs = require('fs');
 const express = require('express');
+const { resolveNaptr } = require('dns');
 const app = express();
 
 const server = app.listen(3001, () => {
     console.log('Start Server; 3001')
+    fs.readFile('./actors.json', (err, data) => {
+        if (err) throw err
+        const actors = () => JSON.parse(data);
+    })
 })
 
-app.get('/api/:actor', async (req, res) => {
-    const {actor} = req.params;
-    let data = {name : "아영", age : "24"};
-    if (actor === 'seoul'){
-        res.send(data);
-    }
+app.get('/api/actors', async (req, res) => {
+    res.sendFile(__dirname + '/actors.json', (err) => {
+        if (err) res.sendStatus(400);
+        else console.log('sending completed');
+    });
 })
+
+app.get("/api/actors/:name", async (req, res) => {
+  const { name } = req.params;
+  fs.readFile("./actors.json", (err, data) => {
+    if (err) throw err;
+    const actors = JSON.parse(data);
+    for (let i = 0; i < actors.length; i++) {
+      if (name === actors[i].idx) {
+        return res.send(actors[i]);
+      } 
+    }
+    return res.status(400).json({
+        status : 'error',
+        error : `요청 parameter ${name}이/가 데이터가 존재하지 않습니다`
+    })
+  });
+});
