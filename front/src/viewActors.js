09b2cf396f0a09a3fab0aea733941a6e26adb35c
dallas <dallas.wty@gmail.com>
2023-02-01 16:29:41 +0900

    actors setting

--- front/src/viewActors.js
+++ front/src/viewActors.js
@@ -1,8 +1,22 @@
+import { useState } from 'react';
+
 function ViewActors(){
+    const [width, setWidth] = useState(window.innerWidth);
+    console.log(width);
     return (
-        <div>
+        <main>
             <h2>View Actors</h2>
-        </div>
+            <div className='actor-flex-box'>
+                <div className='actor-bg_img'></div>
+                <div className='actor-flex-box-container'></div>
+                <div className='actor-flex-box-container'></div>
+            </div>
+            <div className='actor-flex-box'>
+                <div className='actor-flex-box-container'></div>
+                <div className='actor-flex-box-container'></div>
+                <div className='actor-flex-box-container'></div>
+            </div>
+        </main>
     )
 }
 
