f5a494f899b4f035c1e04b77477903b438472dc9
dallas <dallas.wty@gmail.com>
2023-02-02 11:13:25 +0900

    actors desc text appear, disappear

--- front/src/viewActors.js
+++ front/src/viewActors.js
@@ -1,12 +1,33 @@
-import { useState } from 'react';
 import './css/viewActors.css'
+import Actors from './actors.json';
 
 function ViewActors(){
-    // const [width, setWidth] = useState(window.innerWidth);
-    // const [height, setHeight] = useState(0);
+    const actorList = [];
+    const moveDesc = (e) => {
+        document.querySelector(`#${e.target.id}+div`).style.cssText = `display: block`;
+    }
+    const outDesc = (e) => {
+        document.querySelector(`#${e.target.id}+div`).style.cssText = `display: none`;
+    }
+    for(let i = 0; i < Actors.length; i++){
+        const actor = Actors[i]
+        actorList.push(
+            <div key={i} className='actor-sm-img'>
+                <img id={actor.idx} className="actor-sm-img-thumb" src={actor.image} alt={actor.real_name} onMouseOver={moveDesc} onMouseOut={outDesc}></img>
+                <div className='actor-desc'>
+                    <a>{actor.real_name}</a><br />
+                    <span>{actor.char_name} 역</span>
+                </div>
+            </div>
+        )
+    }
+
     return (
         <main>
             <div className='actor-wrap'>
+                {actorList}
+            </div>
+            {/* <div className='actor-wrap'>
                 <div id='box1' className='actor-sm-img'></div>
                 <div className='actor-sm-img'></div>
                 <div className='actor-sm-img'></div>
@@ -47,7 +68,7 @@ function ViewActors(){
                 <div className='actor-sm-img'></div>
                 <div className='actor-sm-img'></div>
                 <div id='box40' className='actor-sm-img'></div>
-            </div>
+            </div> */}
         </main>
     )
 }
